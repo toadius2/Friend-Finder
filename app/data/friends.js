34928941157ed295b7f16b892274bcfac365563d
unknown <animemaster100@gmail.com>
2019-04-18 12:24:31 -0700

    Friend Finder

---
+++ app/data/friends.js
@@ -0,0 +1,39 @@
+//initialize with some 'dummy' data
+var friendList = [
+  {
+    name: 'Joe Schmo',
+    profilePic: 'http://www.mypokecard.com/my/galery/1MtWLmexXqNc.jpg',
+    scores: [
+      "4",
+      "2",
+      "1",
+      "5",
+      "5",
+      "1",
+      "4",
+      "5",
+      "3",
+      "2"
+    ]
+  },
+
+
+  {
+    name: 'Al \"Manbearpig\" Gore',
+    profilePic: 'https://vignette.wikia.nocookie.net/southpark/images/c/c7/Al_Gore_facial_hair.png/revision/latest?cb=20181110130158',
+    scores: [
+      "1",
+      "4",
+      "5",
+      "1",
+      "1",
+      "5",
+      "5",
+      "1",
+      "3",
+      "5"
+    ]
+  },
+]
+
+module.exports = friendList;
